e57bdb72622e26c60917b45305057bc62bbe321f
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-04 15:43:18 -0500

    menu json added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react'
+import MenuSection from './MenuSection'
 import menuItems from '../menu.json'
 
 class Menu extends Component {
 
   render () {
+    const categories = menuItems.categories.map((category, i) => {
+      return <li key={i}>{category.name}</li>
+    })
+
+    const sections = menuItems.categories.map((category, i) => {
+      return <MenuSection name={category.name} items={category.items} key={i} />
+    })
+
     return <div>
       <h2> Our Menu </h2>
 
       <section className='categories'>
         <ul>
-          <li>Appetizers </li>
-          <li>Entrees</li>
-          <li>Deserts</li>
-          <li>Specials</li>
+          {categories}
         </ul>
       </section>
 
-      <section>
-        <h3>Menu</h3>
-        <table>
-          <tbody>
-            <tr>
-              <th>Black pudding Nachos</th>
-              <td>nom nom nom </td>
-              <td>$$</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+      {sections}
     </div>
   }
 }
